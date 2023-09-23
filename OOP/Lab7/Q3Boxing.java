fea19304562649db027f88fed640d4b003369a6a
Ayush-Dharaskar <118068830+Ayush-Dharaskar@users.noreply.github.com>
2023-09-23 09:51:11 +0530

    Q3Boxing.java

---
+++ OOP/Lab7/Q3Boxing.java
@@ -0,0 +1,33 @@
+package Lab7;
+
+public class Q3Boxing {
+    public static void main(String[] args) {
+        Byte b = 127;
+        Short s = 32767;
+        Integer i = 2147483647;
+        Long l = (long) 92233720;
+        Float f = (float) 5.7;
+        Double d = 6.9;
+        Boolean bo = true;
+        Character ch = 'a';
+
+        byte by = b;
+        short sh = s;
+        int in = i;
+        long lo = l;
+        float fl = f;
+        double dou = d;
+        boolean boo = bo;
+        char cha = ch;
+
+        System.out.println("Byte:" + by);
+        System.out.println("Sort:" + sh);
+        System.out.println("Int:" + in);
+        System.out.println("Long:" + lo);
+        System.out.println("Float:" + fl);
+        System.out.println("Double:" + dou);
+        System.out.println("Boolean:" + boo);
+        System.out.println("Character:" + cha);
+
+    }
+}
